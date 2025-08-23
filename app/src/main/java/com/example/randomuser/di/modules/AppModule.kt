11c43810fceecec53fc3ca38e4aa87d211424f4d
Chelyapina <katechelya@gmail.com>
2025-08-23 20:03:55 +0700

    feat(di): add provideIntentLauncher

--- app/src/main/java/com/example/randomuser/di/modules/AppModule.kt
+++ app/src/main/java/com/example/randomuser/di/modules/AppModule.kt
@@ -2,6 +2,8 @@ package com.example.randomuser.di.modules
 
 import android.app.Application
 import android.content.Context
+import com.example.randomuser.presentation.utils.AndroidIntentLauncher
+import com.example.randomuser.presentation.utils.IntentLauncher
 import dagger.Module
 import dagger.Provides
 import javax.inject.Singleton
@@ -12,5 +14,13 @@ class AppModule {
     @Singleton
     fun provideContext(application : Application) : Context {
         return application.applicationContext
+    }
+
+    @Provides
+    @Singleton
+    fun provideIntentLauncher(
+        context : Context ,
+    ) : IntentLauncher {
+        return AndroidIntentLauncher(context)
     }
 }
